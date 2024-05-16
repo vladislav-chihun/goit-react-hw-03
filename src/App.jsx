b462b007410b301606b56d906af518e6ed718623
vladislav-chihun <vladikkosmo@gmail.com>
2024-05-16 14:45:51 +0200

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
+import { useEffect } from "react"
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const contactsDefault = [
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+]
 
+  const [contacts, setContacts] = useEffect(contactsDefault)
+
+
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+  <h1>Phonebook</h1>
+  {/* <ContactForm />
+  <SearchBox />
+  <ContactList /> */}
+</div>
   )
 }
 
