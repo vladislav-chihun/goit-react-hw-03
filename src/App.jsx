9a8e138ee7e695040b0d7c40a3625c1175c1c7e4
vladislav-chihun <vladikkosmo@gmail.com>
2024-05-18 11:27:52 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,16 @@ const contactSchema = yup.object().shape({
 });
 
 function App() {
-  const [contacts, setContacts] = useState([
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ])
+  const [contacts, setContacts] = useState(() => {
+    const storageData = localStorage.getItem("contacts")
+    return storageData ? JSON.parse(storageData) :
+    [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  })
   const [searchValue, setSearchValue] = useState("")
   const handleSearch = (values) => {
     setSearchValue(values);
@@ -31,7 +35,6 @@ function App() {
   const handleCreate = (values,actions) => {
     const newContact = {id:nanoid(), name:values.nameField,number:values.numberField}
     setContacts([...contacts, newContact])
-    localStorage.setItem(newContact)
     actions.resetForm()
   }
   const handleDelete = (id) => {
